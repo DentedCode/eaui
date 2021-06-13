21d696cb4267a91e1f6c69cb31d068199819b6a6
Prem Acharya <prem.acharya@hotmail.com>
2021-06-13 14:52:31 +1000

    apui updated

--- src/apis/loginAPI.js
+++ src/apis/loginAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const rootUrl = "http://localhost:8000/api/v1/";
-const rootUrl = "http://admincmsapi-env.eba-y4wvkyfg.ap-southeast-2.elasticbeanstalk.com/api/v1/";
+const rootUrl = process.env.NODE_ENV !== 'production' ? process.env.REACT_APP_PROD_ROOT_API : "http://localhost:8000/api/v1/";
+ 
 const catApi = rootUrl + "login";
 
 export const loginAPI = frmDt => {
